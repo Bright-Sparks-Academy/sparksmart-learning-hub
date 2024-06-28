6b20894c2f7c79c3542bde9650d77b715fb92002
Danny Chen <155599374+dannywchen@users.noreply.github.com>
2024-06-28 15:14:18 -0400

    Package Fixes for Firestore

--- sparksmart-learning-hub/functions/index.js
+++ sparksmart-learning-hub/functions/index.js
@@ -1,19 +1,11 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// remove
+// const { onRequest } = require("firebase-functions/v2/https");
+// const { logger } = require("firebase-functions");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
