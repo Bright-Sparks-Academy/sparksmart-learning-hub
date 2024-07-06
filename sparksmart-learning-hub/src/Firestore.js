d708cb135dc2e9d85f01941a1fad95bb55f70bb3
MelonGodTier <104312801+MelonGodTier@users.noreply.github.com>
2024-07-05 20:34:58 -0400

    feat: Add addMessage function to Firestore.js
    - Added addMessage function to store new messages in Firestore
      - The function accepts sender, recipient, and content parameters
      - Utilizes serverTimestamp for the message timestamp
      - Includes error handling for Firestore operations
      - Documented with comments and attribution to Tom Wang
    
    Author: Tom Wang, email: tomwang22@yahoo.com>

--- sparksmart-learning-hub/src/Firestore.js
+++ sparksmart-learning-hub/src/Firestore.js
@@ -1,6 +1,14 @@
+// Firestore.js
 import { db } from "./firebaseConfig";
-import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc, serverTimestamp } from "firebase/firestore";
 
+/**
+ * Adds a new document to a specified Firestore collection.
+ * @param {string} collectionName - The name of the Firestore collection.
+ * @param {Object} data - The data to be added to the collection.
+ * @returns {Promise<string>} - The ID of the newly created document.
+ * @throws Will throw an error if the document cannot be added.
+ */
 const addData = async (collectionName, data) => {
   try {
     const docRef = await addDoc(collection(db, collectionName), data);
@@ -11,6 +19,12 @@ const addData = async (collectionName, data) => {
   }
 };
 
+/**
+ * Retrieves all documents from a specified Firestore collection.
+ * @param {string} collectionName - The name of the Firestore collection.
+ * @returns {Promise<Array>} - An array of objects containing the document data.
+ * @throws Will throw an error if the documents cannot be retrieved.
+ */
 const getData = async (collectionName) => {
   try {
     const querySnapshot = await getDocs(collection(db, collectionName));
@@ -22,6 +36,14 @@ const getData = async (collectionName) => {
   }
 };
 
+/**
+ * Updates a document in a specified Firestore collection.
+ * @param {string} collectionName - The name of the Firestore collection.
+ * @param {string} docId - The ID of the document to update.
+ * @param {Object} data - The new data to update in the document.
+ * @returns {Promise<void>}
+ * @throws Will throw an error if the document cannot be updated.
+ */
 const updateData = async (collectionName, docId, data) => {
   try {
     const docRef = doc(db, collectionName, docId);
@@ -32,6 +54,13 @@ const updateData = async (collectionName, docId, data) => {
   }
 };
 
+/**
+ * Deletes a document from a specified Firestore collection.
+ * @param {string} collectionName - The name of the Firestore collection.
+ * @param {string} docId - The ID of the document to delete.
+ * @returns {Promise<void>}
+ * @throws Will throw an error if the document cannot be deleted.
+ */
 const deleteData = async (collectionName, docId) => {
   try {
     const docRef = doc(db, collectionName, docId);
@@ -42,4 +71,27 @@ const deleteData = async (collectionName, docId) => {
   }
 };
 
-export { addData, getData, updateData, deleteData };
+/**
+ * Adds a message to the Firestore messages collection.
+ * Function created by Tom Wang.
+ * @param {string} sender - The email of the sender.
+ * @param {string} recipient - The email of the recipient.
+ * @param {string} content - The content of the message.
+ * @returns {Promise<void>}
+ * @throws Will throw an error if the message cannot be added.
+ */
+const addMessage = async (sender, recipient, content) => {
+  try {
+    await addDoc(collection(db, 'messages'), {
+      sender,
+      recipient,
+      content,
+      timestamp: serverTimestamp()
+    });
+  } catch (error) {
+    console.error('Error adding message:', error);
+    throw error;
+  }
+};
+
+export { addData, getData, updateData, deleteData, addMessage };
