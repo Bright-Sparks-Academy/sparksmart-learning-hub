45fc111b3c9b34c3539d141d19ddde47ba9bcede
MelonGodTier <104312801+MelonGodTier@users.noreply.github.com>
2024-07-05 21:48:47 -0400

    feat: Add state management for messages, message input, and recipient in MessagingPage component. - Used useState hook to manage messages, message input, and recipient state.   - State variables:     - messages: An array to store the list of messages.     - setMessages: Function to update the messages state.     - message: String to store the input message content.     - setMessage: Function to update the message state.     - recipient: String to store the recipient's email.     - setRecipient: Function to update the recipient state. - Detailed comments added for each part of the state management, highlighting the new changes. Author: Tom Wang, tomwang22@yahoo.com

--- sparksmart-learning-hub/src/views/MessagingPage.js
+++ sparksmart-learning-hub/src/views/MessagingPage.js
@@ -1,13 +1,14 @@
 // src/views/MessagingPage.js
 // Author: Tom Wang
 
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 /**
  * PageContainer is the main container for the MessagingPage.
  * It uses styled-components for styling.
  * This component sets the font family, background color, text color, and padding.
+ * Created by Tom Wang.
  */
 const PageContainer = styled.div`
   font-family: 'Gotham', 'Quicksand', sans-serif;
@@ -19,6 +20,7 @@ const PageContainer = styled.div`
 /**
  * Heading is a styled-component for the main heading of the page.
  * It sets the font size, font weight, text color, text alignment, and margin.
+ * Created by Tom Wang.
  */
 const Heading = styled.h1`
   font-size: 2.5rem;
@@ -31,6 +33,7 @@ const Heading = styled.h1`
 /**
  * Subheading is a styled-component for the subheadings on the page.
  * It sets the font size, text color, text alignment, and margin.
+ * Created by Tom Wang.
  */
 const Subheading = styled.h2`
   font-size: 2rem;
@@ -43,8 +46,30 @@ const Subheading = styled.h2`
  * MessagingPage component renders the main messaging interface.
  * It includes placeholders for MessageForm and MessageList components.
  * This component is the entry point for the messaging feature.
+ * Created by Tom Wang.
  */
 const MessagingPage = () => {
+  /**
+   * State for managing the list of messages.
+   * This will be used to store the messages fetched from Firestore.
+   * Created by Tom Wang.
+   */
+  const [messages, setMessages] = useState([]);
+
+  /**
+   * State for managing the input message.
+   * This will be used to store the content of the message being typed by the user.
+   * Created by Tom Wang.
+   */
+  const [message, setMessage] = useState("");
+
+  /**
+   * State for managing the recipient of the message.
+   * This will be used to store the email of the recipient to whom the message will be sent.
+   * Created by Tom Wang.
+   */
+  const [recipient, setRecipient] = useState("");
+
   return (
     <PageContainer>
       <Heading>Messaging</Heading>
