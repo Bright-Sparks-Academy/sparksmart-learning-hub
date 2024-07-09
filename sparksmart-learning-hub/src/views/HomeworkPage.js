b4cd54f08ebab050ade000b9987a1cc2f6c18bde
John Hieu Nguyen <157649243+hnguyen142@users.noreply.github.com>
2024-07-09 12:41:17 -0500

    Fix the Homework page and todoList
    Fix the frontend of the homework page to fit with js and turn in to bright spark website
    Build the navbar link to link the Homework page
    Fix the todo list frontend to fit with the JS file and can work in bright spark website
    
    Author: John Nguyen
    Email: nguyenjohn342@gmail.com
    Date: 09/07
    Time: 12:38

--- sparksmart-learning-hub/src/views/HomeworkPage.js
+++ sparksmart-learning-hub/src/views/HomeworkPage.js
@@ -1,84 +1,185 @@
-// src/components/HomeworkPage.js
-
 import React from 'react';
+import styled from 'styled-components';
 import Todolist from './Todolist';
+
+// Author: John Nguyen
+// Design the the Homework website page 
+
+// make a position of HomeworkPageContainer
+const HomeworkPageContainer = styled.div`
+  position: relative;
+`;
+//move the location of contain to the right way 
+const AbsoluteContainer = styled.div`
+  position: absolute;
+  width: 1385px;
+  height: 681px;
+  top: 154px;
+  left: 20px;
+`;
+//Build the scroll container to scroll the task
+const ScrollContainer = styled.div`
+  overflow-y:auto; /* Equivalent to overscroll-auto */
+  top:20px;
+  position:absolute;
+  width:1320px;
+`;
+//Build yellow background to container
+const YellowBackground = styled.div`
+  position: absolute;
+  width: 1385px;
+  height: 505px;
+  border-radius: 0.5rem;
+  top: 11rem;
+  left: 0;
+  background-color: #f7f03e;
+`;
+//Build white background to container
+const WhiteBackground = styled.div`
+  position: absolute;
+  width: 1350px;
+  height: 470px;
+  top: 202px;
+  border-radius: 1rem;
+  left: 20px;
+  background-color: white;
+`;
+//Build Taskheader container
+const TaskHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem;
+  margin-bottom: 0.5rem;
+  position: absolute;
+  width: 1250px;
+  height: 46px;
+  top: 125px;
+  left: 30px;
+  border-radius: 1.25rem;
+  background-color: #d9d9d9;
+`;
+//Build Taskheader text
+const TaskHeaderItem = styled.span`
+  font-weight: bold;
+  font-size: 0.875rem;
+`;
+//Build TaskHeader button to fit the task header container
+const TaskButton = styled.button`
+  background-color: black;
+  color: #f7f03e;
+  padding: 0.5rem 2.5rem;
+  border-radius: 0.5rem;
+  margin-left: 1rem;
+  cursor: pointer;
+`;
+//Build Headtitle style of the Text Homework
+const HeaderTitle = styled.div`
+  position: absolute;
+  width: 463px;
+  top: 200px;
+  left: 595px;
+  font-weight: bold;
+  font-size: 2.5rem;
+  color: black;
+  letter-spacing: 0.15rem;
+`;
+//Build WelcomBanner to the the Text welcom firstname
+const WelcomeBanner = styled.div`
+  position: absolute;
+  width: 362px;
+  height: 63px;
+  top: 250px;
+  left: 20px;
+  border-radius: 0.5rem;
+  background-color: #f7f03e;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  font-size: 1.5rem;
+  color: black;
+`;
+//Build the another task container
+const TaskLabels = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  width: 1250px;
+  height: 40px;
+  top: 62px;
+  left: 30px;
+  background-color: #d9d9d9;
+  border-radius: 1.25rem;
+  padding: 0.5rem;
+`;
+//Build the label style to fit the container
+const Label = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${(props) => props.width || '48px'};
+  height: 27px;
+  background-color: black;
+  border-radius: 1.25rem;
+  color: white;
+  font-weight: bold;
+  font-size: 1.25rem;
+  margin-top: 10px;
+  left: 20px;
+  position: relative;
+`;
 
 const HomeworkPage = () => {
   return (
-    <div >
-      <div >
-            <div className="absolute w-[1385px] h-[681px] top-[154px] left-[114px]">
-             <div className="absolute w-[1385px] h-[505px] rounded-lg top-44 left-0 bg-[#f7f03e]" />
-          <div className="absolute w-[1350px] h-[470px]  top-[202px] rounded-xl left-[20px] bg-white">
-            
-            <div className="flex justify-between items-center p-2 mb-2  absolute w-[1250px] h-[46px] top-[125px] left-10 rounded-[20px] mx-auto">
-              <div className="flex justify-between items-center  p-2 mb-2 absolute w-[1250px] h-[40px] top-0 left-0 bg-[#d9d9d9] rounded-[20px] mx-auto" > 
-              <span className= "relative left-[20px]">Decimal Practices #1</span>
-              <span className="font-bold text-sm relative left-[40px]">06/23</span>
-              <span className=" font-bold relative left-[55px] text-sm">100</span>
-              
-              <div className="relative  left-[200px] right-[-250px]">
-              <button className=" bg-black text-yellow-400  px-10 py-1  rounded-md mr-10">View</button>
-              </div>
-              <button className=" bg-black text-yellow-400  relative left-[300 px] right-0 px-10 py-1 rounded-md mr-2">Submit</button>
-              
-              </div>
-              
+    <HomeworkPageContainer>
+      <AbsoluteContainer>
+        <YellowBackground />
+        <WhiteBackground>
+          <ScrollContainer>
+          <TaskHeader>
+            <span className="relative left-[20px]">Decimal Practices #1</span>
+            <TaskHeaderItem className="relative left-[40px]">06/23</TaskHeaderItem>
+            <TaskHeaderItem className="relative left-[55px]">100</TaskHeaderItem>
+            <div className="flex items-center">
+              <TaskButton>View</TaskButton>
+              <TaskButton>Submit</TaskButton>
             </div>
+           </TaskHeader>
 
-            <div className="absolute w-[1024px] h-[38px] top-[62px] left-10">
-            <div className="flex justify-between items-center  p-2 mb-2 absolute w-[1250px] h-[40px] top-0 left-0 bg-[#d9d9d9] rounded-[20px] mx-auto" > 
-              <span className= "relative left-[20px]">Decimal Practices #1</span>
-              <span className="font-bold text-sm relative left-[40px]">06/23</span>
-              <span className=" font-bold relative left-[55px] text-sm">100</span>
-              
-              <div className="relative  left-[200px] right-[-250px]">
-              <button className=" bg-black text-yellow-400  px-10 py-1  rounded-md mr-10">View</button>
-              </div>
-              <button className=" bg-black text-yellow-400  relative left-[300 px] right-0 px-10 py-1 rounded-md mr-2">Submit</button>
-              
-              </div>
+          <TaskLabels>
+            <span className="relative left-[20px]">Decimal Practices #1</span>
+            <TaskHeaderItem className="relative left-[40px]">06/23</TaskHeaderItem>
+            <TaskHeaderItem className="relative left-[55px]">100</TaskHeaderItem>
+            <div className="flex items-center">
+              <TaskButton>View</TaskButton>
+              <TaskButton>Submit</TaskButton>
             </div>
-
-            <div className="absolute w-48 h-[27px] top-[18px] left-[63px] bg-black rounded-[20px]">
-              <div className="absolute w-[135px] top-[-2px] left-[23px] font-bold text-white text-xl tracking-[1.20px] leading-[normal] whitespace-nowrap">
-                Assignment
-              </div>
-            </div>
-            
-            <div className="absolute w-48 h-[29px] top-4 left-[352px] bg-black rounded-[20px]" >
-            <div className="absolute w-[135px] top-[-2px] left-[40px] font-bold text-white text-xl tracking-[1.20px] leading-[normal] whitespace-nowrap">
-                Due Date
-              </div>
-            </div>
-            
-            <div className="absolute w-[164px] h-[27px] top-3.5 left-[594px] bg-black rounded-[20px]" >
-            <div className="absolute w-[135px] top-[-2px] left-[50px] font-bold text-white text-xl tracking-[1.20px] leading-[normal] whitespace-nowrap">
-                Points
-              </div>
-            </div>
-
-            <div className="absolute w-[220px] h-[27px] top-3.5 left-[1007px] bg-black rounded-[20px]" >
-            <div className="absolute w-[135px] top-[-2px] left-[50px] font-bold text-white text-xl tracking-[1.20px] leading-[normal] whitespace-nowrap">
-                Submission
-              </div>  
-            </div>
-          </div>
-
+          </TaskLabels>
           
-          <div className="absolute w-[463px] top-0 left-[595px] font-bold text-black text-[40px] tracking-[2.40px] leading-[normal]">
-            Homework
-          </div>
-          <div className="absolute w-[362px] h-[63px] top-[86px] left-8 rounded-lg  bg-[#f7f03e]" >
-          <span className="relative  w-[751px] top-auto bottom-[-15px] left-14  font-bold text-black text-2xl tracking-[1.44px] leading-[normal]"> Welcome {`First Name`}</span>
-          </div>
-        </div>
-       
-      </div>
+
+          <Label style={{ top: '15px', left: '50px', width: '135px' }}>
+            Assignment
+          </Label>
+          <Label style={{ top: '-22px', left: '400px', width: '135px' }}>
+            Due Date
+          </Label>
+          <Label style={{ top: '-60px', left: '660px', width: '164px' }}>
+            Points
+          </Label>
+          <Label style={{ top: '-99px', left: '1040px', width: '220px' }}>
+            Submission
+          </Label>
+          </ScrollContainer>
+        </WhiteBackground>
+      </AbsoluteContainer>
+      <HeaderTitle>HomeWork</HeaderTitle>
+      <WelcomeBanner>Welcome {`First Name`}</WelcomeBanner>
       <Todolist />
-    </div>
-    
+    </HomeworkPageContainer>
   );
-}
+};
 
 export default HomeworkPage;
+
+
--- sparksmart-learning-hub/src/views/Todolist.js
+++ sparksmart-learning-hub/src/views/Todolist.js
@@ -1,39 +1,133 @@
 import React from 'react';
+import styled from 'styled-components';
+
+//Build the Todolist part like this picture
+
+//Build the Todoliscontainer shape and background color yellow
+const TodolistContainer = styled.div`
+  position: absolute;
+  width: 653px;
+  height: 753px;
+  top: 854px;
+  left: 20px;
+  background-color: #fbbf24; /* Equivalent to bg-yellow-400 */
+  border-radius: 1rem; /* Equivalent to rounded-xl */
+  padding: 1.5rem; /* Equivalent to p-6 */
+  overflow: hidden;
+`;
+//Build the TodolistInner with background white to fit the container
+const TodolistInner = styled.div`
+  position: relative;
+  width: 563px;
+  height: 682px;
+  top: 1rem; /* Equivalent to top-4 */
+  left: 35px;
+  background-color: white;
+  padding: 1.5rem; /* Equivalent to p-6 */
+  border-radius: 1rem; /* Equivalent to rounded-lg */
+  box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.1); /* Equivalent to shadow-inner */
+`;
+//Build the Header to fit the text Todolist , title , and navbutton inside the container
+const Header = styled.div`
+  text-align: center;
+  margin-bottom: 1rem; /* Equivalent to mb-4 */
+`;
+//Build Title to fit the container
+const Title = styled.h2`
+  font-size: 1.5rem; /* Equivalent to text-2xl */
+  font-weight: bold;
+`;
+//Build the Subtitle to fit the container
+const SubTitle = styled.p`
+  font-size: 1.125rem; /* Equivalent to text-lg */
+  font-weight: 600; /* Equivalent to font-semibold */
+`;
+//Build the NavButton to fit in calender
+const NavButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 1rem; /* Equivalent to mb-4 */
+`;
+//Build the style of Navbutton
+const NavButton = styled.button`
+  background-color: black;
+  color: white;
+  border-radius: 50%;
+  width: 2rem; /* Equivalent to w-8 */
+  height: 2rem; /* Equivalent to h-8 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 0.5rem; /* Equivalent to mr-2 and ml-2 */
+`;
+//Build the title of calendar
+const WeekDisplay = styled.p`
+  font-weight: 600; /* Equivalent to font-semibold */
+  background-color: #f7f03e; /* Equivalent to bg-primary-color */
+  color: black;
+  padding: 0.25rem 1.25rem; /* Equivalent to py-1 and px-5 */
+  font-size: 1rem; /* Equivalent to text-base */
+  text-align: center;
+  cursor: pointer;
+  margin-right: 1rem; /* Equivalent to mr-4 */
+  outline: none;
+  box-shadow: 0 1px black; /* Equivalent to shadow-[0_1px_black] */
+  border-radius: 0; /* Equivalent to rounded-[0px] */
+`;
+//Build the scroll container to fit the box of task container
+const ScrollContainer = styled.div`
+  overflow-y: auto; /* Equivalent to overscroll-auto */
+`;
+//Build the Taskcontainer 
+const TaskContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #e5e7eb; /* Equivalent to bg-gray-200 */
+  padding: 0.5rem; /* Equivalent to p-2 */
+  margin-bottom: 0.5rem; /* Equivalent to mb-2 */
+  border-radius: 0.375rem; /* Equivalent to rounded-md */
+  width: 456px;
+  margin: 0 auto; /* Equivalent to mx-auto */
+  position:relative;
+`;
+//Build the Task Button to fit the container
+const TaskButton = styled.button`
+  background-color: black;
+  color: #f59e0b; /* Equivalent to text-amber-400 */
+  padding: 0.25rem 0.5rem; /* Equivalent to px-2 and py-1 */
+  cursor: pointer;
+  border-radius: 0.375rem; /* Equivalent to rounded-md */
+`;
 
 const Todolist = () => {
-      
   return (
-    <div className="absolute w-[653px] h-[753px] top-[854px] left-[189px] bg-yellow-400 rounded-xl p-6 overflow-hidden">
-      <div className="relative w-[563px] h-[682px] top-4 left-[35px] bg-white p-6 rounded-lg  shadow-inner">
-        <div className="text-center mb-4">
-          <h2 className="text-2xl font-bold">To-Do List</h2>
-          <p className="text-lg font-semibold">June 2024</p>
-          <div className="flex justify-center items-center mb-4">
-          <button className="bg-black text-white rounded-full w-8 h-8 flex items-center justify-center mr-2 ">{'<'}</button>
-          <p className=" inline  font-semibold bg-primary-color  text-black py-1 px-5 text-base text-center cursor-pointer mr-4 outline-none shadow-[0_1px_black] rounded-[0px]">
-           <span>Week 3: 06/17 - 06/23</span>
-          </p>
-          <button className="bg-black text-white rounded-full w-8 h-8 flex items-center justify-center ml-2">{'>'}</button>
-          </div>
-        </div>
-       
-        <div className= "overscroll-auto">
-          <div className="flex justify-between items-center bg-gray-200 p-2 mb-2 rounded-md w-[456px] mx-auto">
+    <TodolistContainer>
+      <TodolistInner>
+        <Header>
+          <Title>To-Do List</Title>
+          <SubTitle>June 2024</SubTitle>
+          <NavButtonsContainer>
+            <NavButton>{'<'}</NavButton>
+            <WeekDisplay>Week 3: 06/17 - 06/23</WeekDisplay>
+            <NavButton>{'>'}</NavButton>
+          </NavButtonsContainer>
+        </Header>
+        <ScrollContainer>
+          <TaskContainer>
             <span>Decimal Practices #1-3</span>
             <span className="text-sm">06/18</span>
-            <button className="bg-black text-amber-400 px-2 py-1  cursor-pointer rounded-md">View</button>
-          </div>
-          <div className="flex justify-between items-center bg-gray-200 p-2  mb-2 rounded-md w-[456px] mx-auto">
+            <TaskButton>View</TaskButton>
+          </TaskContainer>
+          <TaskContainer  style={{ top: '5px', left: '1px', width: '460px' }}>
             <span>Fraction Multiplication</span>
             <span className="text-sm">06/22</span>
-            <button className="bg-black text-amber-400 px-2 cursor-pointer py-1 rounded-md">View</button>
-          </div>
-        </div>
-      </div>
-    </div>
-        
-      
-    
+            <TaskButton>View</TaskButton>
+          </TaskContainer>
+        </ScrollContainer>
+      </TodolistInner>
+    </TodolistContainer>
   );
 };
 
